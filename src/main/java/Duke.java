24894be7c655b0118465926d9d78c8ae5164878e
Wira Azmoon <e0014172@u.nus.edu>
2020-08-09 22:27:12 +0800

    Add command echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String horLine = "____________________________________________________________\n";
 
-        String greeting = "____________________________________________________________\n" +
+        String greeting = horLine +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n";
+                horLine;
 
-        String bye = " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        String bye = horLine + " Bye. Hope to see you again soon!\n" + horLine;
 
-        System.out.println(greeting + bye);
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(greeting);
+        line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(horLine + ' ' + line + '\n' + horLine);
+            line = in.nextLine();
+        }
+
+        System.out.println(bye);
+
     }
 }
