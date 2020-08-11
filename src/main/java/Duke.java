bb66ad95f6517123f3be48651b5fc96f088ec241
Wira Azmoon <e0014172@u.nus.edu>
2020-08-11 16:16:34 +0800

    Add todo list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,47 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String horLine = "____________________________________________________________\n";
+        // Horizontal Line reused
+        String horLine = "____________________________________________________________";
 
+        // Greeting and Farewell at start and end
         String greeting = horLine +
-                " Hello! I'm Duke\n" +
+                "\n Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 horLine;
 
-        String bye = horLine + " Bye. Hope to see you again soon!\n" + horLine;
+        String bye = horLine + "\n Bye. Hope to see you again soon!\n" + horLine;
 
+        // To do list and number of items tracker
+        String[] toDoList = new String[100];
+        int numItems = 0;
+
+        // Scanner class for user input
         String line;
         Scanner in = new Scanner(System.in);
 
+        // Print greeting
         System.out.println(greeting);
+
+        // Parsing of user input
         line = in.nextLine();
 
         while (!line.equals("bye")) {
-            System.out.println(horLine + ' ' + line + '\n' + horLine);
+            // Print toDoList
+            if (line.equals("list")) {
+                System.out.println(horLine);
+                for (int i = 1; i <= numItems; i++) {
+                    System.out.println(" " + i + ". " + toDoList[i-1] );
+                }
+                System.out.println(horLine);
+            } else { // Add to toDoList
+                toDoList[numItems++] = line;
+                System.out.println(horLine + "\n added: " + line + '\n' + horLine);
+            }
             line = in.nextLine();
         }
 
+        // Print farewell
         System.out.println(bye);
 
     }
