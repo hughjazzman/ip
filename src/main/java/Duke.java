990ef2fdd9221f2580f408bcc8c441ede0853452
Wira Azmoon <e0014172@u.nus.edu>
2020-08-30 03:56:23 +0800

    Add basic error catching

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,9 @@ public class Duke {
     private static final String COMMAND_TODO = "todo";
     private static final String COMMAND_DEADLINE = "deadline";
     private static final String COMMAND_EVENT = "event";
+    /** Parameters for commands **/
+    private static final String PARAM_BY = "/by";
+    private static final String PARAM_AT = "/at";
 
     public static void main(String[] args) {
         // Print logo and greeting
@@ -28,7 +31,7 @@ public class Duke {
 
         // Parsing of user input
         do {
-            line = in.nextLine();
+            line = in.nextLine().strip();
             execute(taskManager, line);
         } while (!line.equals("bye"));
 
@@ -66,7 +69,38 @@ public class Duke {
 
     private static void printInvalid() {
         printHorizontalLine();
-        System.out.println(" Invalid Command!");
+        System.out.println(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        printHorizontalLine();
+    }
+
+    private static void printEmpty(String command) {
+        printHorizontalLine();
+        System.out.println(" ☹ OOPS!!! The description of a " + command + " cannot be empty.");
+        printHorizontalLine();
+    }
+
+    private static void printList(TaskManager taskManager) {
+        printHorizontalLine();
+        System.out.println("Here are the tasks in your list:");
+        taskManager.listTasks();
+        printHorizontalLine();
+    }
+
+    private static void printWrongFormatInteger() {
+        printHorizontalLine();
+        System.out.println(" The index of the task must be an integer.");
+        printHorizontalLine();
+    }
+
+    private static void printWrongFormatTask(String command, String parameter) {
+        printHorizontalLine();
+        System.out.println(" " + command + " requires the " + parameter + " parameter");
+        printHorizontalLine();
+    }
+
+    private static void printInvalidTask() {
+        printHorizontalLine();
+        System.out.println(" The task ID does not exist!");
         printHorizontalLine();
     }
 
@@ -85,7 +119,7 @@ public class Duke {
         // Find position of first space
         int spacePos = line.indexOf(" ");
         // command is first word
-        String command = spacePos > 0 ? line.substring(0, spacePos) : line;
+        String command = spacePos > 0 ? line.substring(0, spacePos).strip() : line;
         String num;
 
         switch (command) {
@@ -93,7 +127,7 @@ public class Duke {
             printList(taskManager);
             break;
         case COMMAND_DONE:
-            num = line.substring(spacePos + 1);
+            num = line.substring(spacePos + 1).strip();
             execDone(taskManager, num);
             break;
         case COMMAND_BYE:
@@ -109,16 +143,29 @@ public class Duke {
         }
     }
 
-    private static void printList(TaskManager taskManager) {
-        printHorizontalLine();
-        System.out.println("Here are the tasks in your list:");
-        taskManager.listTasks();
-        printHorizontalLine();
-    }
-
+    /**
+     * Executes marking a task as done
+     *
+     * @param taskManager TaskManager object.
+     * @param num ID number of task in list.
+     */
     private static void execDone(TaskManager taskManager, String num) {
-        int id = Integer.parseInt(num);
-        Task task = taskManager.markAsDone(id);
+        int id;
+
+        try {
+            id = Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            printWrongFormatInteger();
+            return;
+        }
+
+        Task task;
+        try {
+            task = taskManager.markAsDone(id);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            printInvalidTask();
+            return;
+        }
         printHorizontalLine();
         System.out.println(" Nice! I've marked this task as done:");
         System.out.println(" " + task.toString());
@@ -134,31 +181,47 @@ public class Duke {
      */
     private static void execAddTask(TaskManager taskManager, String command, String line) {
         // Position of task description, /by marker, and /at marker
-        int descPos = line.indexOf(" ") + 1, byPos, atPos;
+        int descPos = line.indexOf(" "), byPos, atPos;
+        String descriptionParam, description;
+
         // Check for blank description
-        if (descPos <= 0) {
-            printInvalid();
+        try {
+            descriptionParam = line.substring(descPos).strip();
+        } catch (StringIndexOutOfBoundsException e) {
+            printEmpty(command);
             return;
         }
 
-        String description = line.substring(descPos);
         String by, at;
         Task task;
 
         switch (command) {
         case COMMAND_TODO:
-            task = taskManager.addTodo(description);
+            task = taskManager.addTodo(descriptionParam);
             break;
         case COMMAND_DEADLINE:
-            byPos = description.indexOf("/by");
-            by = description.substring(byPos + 4);
-            description = description.substring(0, byPos - 1);
+            byPos = descriptionParam.indexOf(PARAM_BY);
+
+            try {
+                String[] details = parseTask(descriptionParam, COMMAND_DEADLINE, PARAM_BY, byPos);
+                by = details[0];
+                description = details[1];
+            } catch (Exception e) {
+                return;
+            }
+
             task = taskManager.addDeadline(description, by);
             break;
         case COMMAND_EVENT:
-            atPos = description.indexOf("/at");
-            at = description.substring(atPos + 4);
-            description = description.substring(0, atPos - 1);
+            atPos = descriptionParam.indexOf(PARAM_AT);
+            try {
+                String[] details = parseTask(descriptionParam, COMMAND_EVENT, PARAM_AT, atPos);
+                at = details[0];
+                description = details[1];
+            } catch (Exception e) {
+                return;
+            }
+
             task = taskManager.addEvent(description, at);
             break;
         default:
@@ -172,5 +235,89 @@ public class Duke {
                 "\n Now you have " + taskManager.getTasksCount() +
                 " tasks in the list.");
         printHorizontalLine();
+    }
+
+    /**
+     * Parses the parameters of the user input. Used in parseTask.
+     *
+     * @param descriptionParam User input after the command.
+     * @param command Command name.
+     * @param param Parameter name.
+     * @param paramPos Parameter index in descriptionParam.
+     * @return paramDetails User input for the param.
+     * @throws StringIndexOutOfBoundsException If paramPos is < 0 (missing param)
+     *         or paramPos is > descriptionParam.length() (blank param)
+     */
+    private static String parseParam(
+            String descriptionParam, String command, String param, int paramPos)
+            throws StringIndexOutOfBoundsException {
+        String paramDetails;
+        try {
+            paramDetails = descriptionParam.substring(paramPos + param.length() + 1).strip();
+            return paramDetails;
+        } catch (StringIndexOutOfBoundsException e) {
+            // Check that there is a parameter
+            if (paramPos < 0) {
+                printWrongFormatTask(command, param);
+            } else { // Check for blank parameter
+                printEmpty(command + " " + param + " parameter");
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * Parses the description of the user input. Used in parseTask.
+     *
+     * @param descriptionParam User input after the command.
+     * @param command Command name.
+     * @param param Parameter name.
+     * @param paramPos Parameter index in descriptionParam.
+     * @return description User input for the command description.
+     * @throws StringIndexOutOfBoundsException If paramPos <= 0 (missing description)
+     */
+    private static String parseDesc(
+            String descriptionParam, String command, String param, int paramPos)
+            throws StringIndexOutOfBoundsException {
+        String description;
+        try {
+            description = descriptionParam.substring(0, paramPos - 1).strip();
+            return description;
+        } catch (StringIndexOutOfBoundsException e) {
+            printEmpty(command);
+            throw e;
+        }
+    }
+
+    /**
+     * Parses the entire task given as user input with description and parameters.
+     * Used in execAddTask.
+     *
+     * @param descriptionParam User input after the command.
+     * @param command Command name.
+     * @param param Parameter name.
+     * @param paramPos Parameter index in descriptionParam.
+     * @return details String array of paramDetails and description of task.
+     * @throws RuntimeException If parsed description is blank.
+     */
+    private static String[] parseTask(
+            String descriptionParam, String command, String param, int paramPos)
+            throws RuntimeException {
+        String paramDetails, description;
+        String[] details = new String[2];
+
+        paramDetails = parseParam(descriptionParam, command, param, paramPos);
+        description = parseDesc(descriptionParam, command, param, paramPos);
+
+        if (description.isBlank()) {
+            printEmpty(command);
+            throw new RuntimeException();
+        }
+
+        details[0] = paramDetails;
+        details[1] = description;
+
+        return details;
+
     }
 }
